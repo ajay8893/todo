063d44f5317572cf155409b12d1afcfc440e9ea4
ajay8893 <100362918+ajay8893@users.noreply.github.com>
2025-07-30 17:07:07 +0530

    defining urls and views

---
+++ todo_project/todo/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def task_list(request):
+    return render
+
+
+def task_create(request):
+    return render
+
+
+def task_update(request):
+    return render
+
+
+def task_delete(request):
+    return render
+
+
+def task_toggle_complete(request):
+    return render
